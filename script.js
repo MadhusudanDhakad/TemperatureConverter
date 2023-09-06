a8f37c67b7d301cafe2921320da391a326c639ac
Madhusudan Dhakad <93048008+MadhusudanDhakad@users.noreply.github.com>
2023-09-06 09:27:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function convertToFar() {
+    const celsiusInput = document.getElementById('celsius');
+    const fahrenheitInput = document.getElementById('fahrenheit');
+    const celsiusValue = parseFloat(celsiusInput.value);
+    const fahrenheitValue = (celsiusValue * 9/5) + 32;
+    fahrenheitInput.value = fahrenheitValue.toFixed(2);
+  }
+  
+  function convertToCelsius() {
+    const celsiusInput = document.getElementById('celsius');
+    const fahrenheitInput = document.getElementById('fahrenheit');
+    const fahrenheitValue = parseFloat(fahrenheitInput.value);
+    const celsiusValue = (fahrenheitValue - 32) * 5/9;
+    celsiusInput.value = celsiusValue.toFixed(2);
+  }
+  
